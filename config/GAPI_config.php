7b3b3cbc126f7adf8a0e5541bf94eb05e5c05f73
cherinolero <cherinolero@gmail.com>
2014-11-29 23:43:19 +0100

    Inclusion of Google API examples and config file for API credentials. autoload.php moved to vendor folder with the rest of the API

---
+++ config/GAPI_config.php
@@ -0,0 +1,8 @@
+<?php
+$GAPI_config = array(
+    'client_id' => '',
+    'client_secret' => '',
+    'redirect_uri' => 'http://localhost/examplesAPI/',
+    'apiKey' => '',
+    'service_account_name' => '',
+    );
